62df53b97431f163b818b52e8a7e9bdea1dd7a3b
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-01-30 16:09:17 +0700

    Add color palete

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 function Home() {
   return (
-    <div className="bg-red-500">
-      Welcome to NextJS
+    <div className="flex items-center justify-center min-h-screen">
+      <div class="w-1/2">
+        <div class="bg-white shadow p-6 rounded">
+          <div class="flex flex-wrap">
+            <div class="bg-true-gray-500 w-10 h-10 rounded"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
