3238ce6dc33618ca778ffd09dbd41f792e9e1e03
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-01-30 23:58:03 +0700

    Dark Mode with Media

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,26 @@ import Ring from "../components/Ring"
 
 function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="antialiased flex items-center justify-center min-h-screen bg-gray-200 dark:bg-gray-700">
       <div className="w-5/12">
-        <Card/>
-        <div className="my-20"></div>
-        <Ring/>
+        <div className="bg-white dark:bg-gray-800 shadow p-4 rounded-lg mb-6 flex items-center justify-between">
+          <div className="font-semibold text-lg text-gray-800 dark:text-white">Switcher</div>
+          <div>
+            <button className="w-4 h-4 focus:outline-none bg-gray-200 rounded-full mr-2"></button>
+            <button className="w-4 h-4 focus:outline-none bg-black rounded-full"></button>
+          </div>
+        </div>
+        <div className="bg-white dark:bg-gray-800 shadow rounded-lg overflow-hidden">
+          <div className="px-10 py-8">
+            <h1 className="text-2xl font-bold text-gray-800 dark:text-white mb-6">Lorem ipsum dolor sit amet.</h1>
+            <div className="leading-relaxed text-gray-500 dark:text-gray-300 text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, maiores magni tempore consequuntur voluptas voluptate numquam? Odio iure temporibus optio quasi aperiam magnam architecto cum!
+            </div>
+          </div>
+          <div className="px-10 py-6 border-t bg-white dark:bg-gray-900 border-gray-200 dark:border-gray-900 dark:text-gray-200">
+            Lorem ipsum dolor sit amet.
+          </div>
+        </div>
       </div>
     </div>
   )
